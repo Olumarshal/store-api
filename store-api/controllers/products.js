7338ada86a8ce00e78bc6334d1c0115d33eaaf5d
Marshal Olu <olmarsh.192@gmail.com>
2022-11-26 08:34:28 +0100

    added pagination and select functionality

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -5,7 +5,7 @@ const getAllProductsStatic = async (req, res) => {
     res.status(200).json({ products, ndHits: products.length })
 }
 const getAllProducts = async (req, res) => {
-    const { featured, company } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -14,8 +14,28 @@ const getAllProducts = async (req, res) => {
     if (company) {
         queryObject.company = company
     }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+    let result = Product.find(queryObject)
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
 
-    const products = await Product.find(queryObject);
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList)
+    }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page -1) * limit;
+
+    result = result.skip(skip).limit(limit)
+
+    const products = await result;
     res.status(200).json({ products, ndHits: products.length })
 }
 
