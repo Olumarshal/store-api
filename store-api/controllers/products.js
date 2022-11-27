b7eefb8e75ec02e287533b9ac812fa0aea7b82d2
Marshal Olu <olmarsh.192@gmail.com>
2022-11-27 08:18:45 +0100

    refactored the code

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,11 +1,8 @@
 const Product = require('../models/product')
 
-const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({})
-    res.status(200).json({ products, ndHits: products.length })
-}
+
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort, fields } = req.query
+    const { featured, company, name, sort, fields, numericFilters } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -16,6 +13,27 @@ const getAllProducts = async (req, res) => {
     }
     if (name) {
         queryObject.name = { $regex: name, $options: 'i' }
+    }
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<':'$lt',
+            '<=':'$lte'
+        }
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`
+        )
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item) => {
+            const [field,operator,value] = item.split('-')
+            if(options.includes(field)) {
+                queryObject[field] = { [operator]: Number(value) }
+            }
+        })
     }
     let result = Product.find(queryObject)
     if (sort) {
@@ -39,7 +57,4 @@ const getAllProducts = async (req, res) => {
     res.status(200).json({ products, ndHits: products.length })
 }
 
-module.exports = {
-    getAllProductsStatic,
-    getAllProducts,
-}
+module.exports = getAllProducts
--- store-api/routes/products.js
+++ store-api/routes/products.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { getAllProductsStatic, getAllProducts } = require('../controllers/products')
+const { getAllProducts } = require('../controllers/products')
 
 
 router.route('/').get(getAllProducts)
-router.route('/static').get(getAllProductsStatic)
 
 
 module.exports = router
